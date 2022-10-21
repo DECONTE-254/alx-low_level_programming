e414258db8a9c74a01e3c420622cef756578f579
DECONTE-254 <okothalex69@gmail.com>
2022-10-21 12:30:14 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file - main.h
+ * Author: Alex deconte devps
+ * Desc:Header contains prototype for all functions used in the 0x04-more_functions_nested_loops
+ */
+
+int _putchar(char c);
+
+#endif
+
