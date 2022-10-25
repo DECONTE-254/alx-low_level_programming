573a4a079cac34417bd84defa8aa7552131eba98
DECONTE-254 <okothalex69@gmail.com>
2022-10-25 13:38:24 -0700

    task0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * prototypes files
+ * import form here
+ */
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
