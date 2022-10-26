f1098ea99187828f56e0df003f154a3f64f6084c
DECONTE-254 <okothalex69@gmail.com>
2022-10-25 21:07:53 -0700

    task 8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,17 @@
 void puts_half(char *str)
 {
 	int len = 0;
-
 	while (*str != '\0')
 	{
 		len++;
 		str++;
 	}
-
-	str -= (len / 2);
+        str -= (len / 2);
 	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;
 	}
-	
-	_putchar('\n');
+		_putchar('\n');
 }
 		
