d2169d15a652f963415e98c9e99b033c323458ef
DECONTE-254 <okothalex69@gmail.com>
2023-02-16 09:21:16 -0800

    gcc 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in C
- * code by alex ict
- * Return: 0
+ * main - A program that prints the size of various computer types
+ * Return: 0 (Success)
  */
 int main(void)
 {
