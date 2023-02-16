802eca950bdde045f4fcc9d7dca52e073a7dc427
DECONTE-254 <okothalex69@gmail.com>
2023-02-16 10:00:48 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - print a c program using write
- *
- * Return: 1
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
