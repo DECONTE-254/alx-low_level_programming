26d9369f0b64b9c714307beab12e46ac3ed41475
DECONTE-254 <okothalex69@gmail.com>
2023-02-22 17:26:28 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,6 +3,7 @@
 /**
  * print_last_digit - Prints the last digit of a number
  * @n: The value of the last digit of a number
+ * Return: value of the last digit of number
  */
 
 int print_last_digit(int n)
@@ -10,9 +11,9 @@ int print_last_digit(int n)
 	int last;
 
 	last = n % 10;
-	if (last <0)
+	if (last < 0)
 	{
-		last = last * -1;
+	last = last * -1;
 	}
 	_putchar(last + '0');
 	return (last);
