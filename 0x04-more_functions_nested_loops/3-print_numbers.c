94d97b3f43768ce555d2d652c9ae42b06dd47383
DECONTE-254 <okothalex69@gmail.com>
2023-02-24 02:12:16 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *print_number - print num from 0-9
@@ -11,7 +10,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	putchar(c);
+	_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
